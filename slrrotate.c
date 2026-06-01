b22cd1cd408cbd166dd5d3bc8191b79480da83a3
pariarora309 <pariarora309@gmail.com>
2026-06-02 00:09:27 +0530

    initial commit

---
+++ slrrotate.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[100], newstr[100];
+    int n, i;
+    scanf("%99s", str);
+    scanf("%d", &n);
+    char d;
+    scanf(" %c", &d);
+    int len = strlen(str);
+    n = n % len;
+    if (d == 'L')
+    {
+        for (i = 0; i < len; i++)
+        {
+            newstr[i] = str[(i + n) % len];
+        }
+    }
+    else if (d == 'R')
+    {
+        for (i = 0; i < len; i++)
+        {
+            newstr[i] = str[(i - n + len) % len];
+        }
+    }
+
+    newstr[len] = '\0';
+    for (i = 0; i < n && i < len; i++)
+    {
+        if (islower(newstr[i]))
+            newstr[i] = toupper(newstr[i]);
+        else if (isupper(newstr[i]))
+            newstr[i] = tolower(newstr[i]);
+    }
+    printf("%s\n", newstr);
+    return 0;
+}
